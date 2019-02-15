98e8542285988c9eb69d4a07a3bbb3bea8aea075
Visual GMQ <2142587070@qq.com>
2019-02-15 23:10:53 +0800

    added operators

---
+++ basic_syntax/operators/operators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class operators{
+public static void main(String[] args){
+	Integer i1 = new Integer(31);	
+	Integer i2 = new Integer(31);	
+	System.out.println("i1:"+i1+",i2:"+i2);
+	System.out.println("i1==i2:"+(i1==i2));
+	System.out.println("i1!=i2:"+(i1!=i2));
+	System.out.println("i1.equals(i2):"+i1.equals(i2));
+}
+};
