a02037e01211e446e5335848e03661ffbdee8685
Visual GMQ <2142587070@qq.com>
2019-02-16 13:44:02 +0800

    added packgae

---
+++ basic_syntax/package/Main.java
@@ -0,0 +1,8 @@
+import main.ob.pk;
+
+public class Main{
+	public static void main(String[] args){
+		pk p = new pk();
+		System.out.println(p);
+	}
+};
---
+++ basic_syntax/package/main/ob/pk.java
@@ -0,0 +1,8 @@
+package main.ob;
+
+public class pk{
+	public pk(){}
+	public String toString(){
+		return "the class is "+this.getClass();
+	}
+};
