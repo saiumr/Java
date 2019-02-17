32b7008e9e586d1b3472569e48599149a21e0505
Visual GMQ <2142587070@qq.com>
2019-02-17 14:48:14 +0800

    added String

---
+++ basic_syntax/String/ReadMe.md
@@ -0,0 +1,6 @@
+Java中的String类是不可以改变的，也就是说下面代码是不会产生效果的：
+```java
+String str = "hello world";
+str[2]='r';
+```
+其他的方法请见Dash，String同时也有正则表达式的函数，例如match和split。这些都请见Dash。
---
+++ basic_syntax/String/string.java
@@ -0,0 +1,7 @@
+class string{
+public static void main(String[] args){
+	String str1 = "Hello World";
+	String str2 = "yes";
+	System.out.format("%s,%s",str1,str2); //or use System.out.printf();
+}
+};
